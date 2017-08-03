adbbbe4358eca4e6f334da4d3a0b64f11d38386d
Bao Dang <baocancode@gmail.com>
2017-08-03 22:20:13 +1000

    feat: create client to connect ssh and handle proxy

---
+++ packages/slrun-client/lib/index.js
@@ -0,0 +1,97 @@
+require('ws')
+require('primus-msgpack')
+const http = require('http')
+const express = require('express')
+const createTunnel = require('reverse-tunnel-ssh')
+const { EventEmitter2 } = require('eventemitter2')
+const Primus = require('primus')
+const substream = require('substream')
+const bodyParser = require('body-parser')
+
+const localhost = '127.0.0.1'
+const jsonParser = bodyParser.json()
+
+module.exports = function createClient (options) {
+  const { name, executor, apiClient, tunnelAuth, dashboardUrl } = options
+  const client = new EventEmitter2()
+  const PrimusSocket = Primus.createSocket({
+    transformer: 'websockets',
+    parser: 'msgpack',
+    plugin: { substream }
+  })
+  const dashboardClient = dashboardUrl ? new PrimusSocket(dashboardUrl) : null
+  if (dashboardClient) {
+    dashboardClient.on('open', () => {
+      console.log('started connection')
+      reportService()
+    })
+    client.on('create-service', reportService)
+    dashboardClient.on('end', () => {
+      console.log('stopped connection')
+    })
+    dashboardClient.on('reconnect', () => {
+      console.log('attempting to reconnect')
+    })
+    dashboardClient.on('error', (err) => {
+      console.error(err)
+    })
+  }
+  const app = express()
+  const requests = []
+  app.get('/__slrun__/requests', (req, res) => {
+    // BD: TODO add authentication
+    res.json(client.requests)
+  })
+  app.post('/__slrun__/requests', (req, res) => {
+    // BD: TODO add authentication
+    jsonParser(req, res, () => {
+      requests.push(req.body)
+      res.json('OK')
+    })
+  })
+  app.use(executor)
+  const httpServer = http.createServer(app)
+  httpServer.on('upgrade', executor.upgrade)
+  Object.assign(client, { app, httpServer, dashboardClient, requests })
+  httpServer.listen(0, localhost, async () => {
+    const sshPoint = (await apiClient.sshPoints.get('/random')).data
+    const localPort = httpServer.address().port
+    const tunnelConfig = Object.assign({
+      dstHost: '0.0.0.0',
+      dstPort: 0,
+      srcHost: localhost,
+      srcPort: localPort,
+      // BD: must set this to use keyboard-interactive, see below
+      tryKeyboard: !!tunnelAuth.password
+    }, sshPoint, tunnelAuth)
+    const tunnel = createTunnel(tunnelConfig, (err) => {
+      if (err) {
+        throw err
+      }
+    })
+    Object.assign(client, { tunnel })
+    client.emit('create-tunnel')
+    // BD: Mac OS X uses the keyboard-interactive mechanism to display a password prompt
+    // https://github.com/mscdex/ssh2/issues/238
+    if (tunnelAuth.password) {
+      tunnel.on('keyboard-interactive', (name, instructions, lang, prompts, finish) => {
+        if (prompts[0].prompt.indexOf('Password:') !== -1) {
+          finish([tunnelAuth.password])
+        }
+      })
+    }
+    tunnel.on('forward-in', async (remotePort) => {
+      const service = (await apiClient.services.post('/', { name, sshPoint, remotePort })).data
+      Object.assign(client, { service, localPort })
+      client.emit('create-service')
+    })
+    // BD: TODO reconnect when tunnel is closed
+  })
+  return client
+  function reportService () {
+    if (dashboardClient && client.service) {
+      const { service: { id, url }, localPort } = client
+      dashboardClient.substream('REPORT_SERVICE').write({ id, url, localPort })
+    }
+  }
+}
---
+++ packages/slrun-client/lib/parseOptions.js
@@ -0,0 +1,84 @@
+const path = require('path')
+const parseProcessArgv = require('yargs-parser')
+
+module.exports = function parseOptions (processArgv) {
+  const argv = parseProcessArgv(processArgv, {
+    alias: {
+      proxy: 'p',
+      static: 's',
+      function: 'f'
+    }
+  })
+  const { name = path.basename(process.cwd()) } = argv
+  return {
+    name,
+    entries: parseEntries(argv)
+  }
+}
+
+function parseEntries (argv) {
+  const allEntries = Object.keys(argv)
+  .map((key) => {
+    const values = isArray(argv[key]) ? argv[key] : [argv[key]]
+    if (key === 'proxy') {
+      return values.map((value) => createUrlEntry(value, 'PROXY'))
+    }
+    if (key === 'static') {
+      return values.map((value) => createPathEntry(value, 'STATIC'))
+    }
+    if (key === 'function') {
+      return values.map((value) => createPathEntry(value, 'FUNCTION'))
+    }
+    return null
+  })
+  .reduce((entries, subEntries) => {
+    if (subEntries) {
+      subEntries.forEach((subEntry) => {
+        if (subEntry) {
+          entries.push(subEntry)
+        }
+      })
+    }
+    return entries
+  }, [])
+  return allEntries
+}
+
+function createUrlEntry (value, mode) {
+  if (isNumber(value)) {
+    return { mode, path: '/', base: normalizeUrl(`127.0.0.1:${value}`) }
+  }
+  const parts = value.split(':')
+  if (parts.length === 1) {
+    return { mode, path: '/', base: normalizeUrl(parts[0]) }
+  }
+  return { mode, path: normalizeEntryPath(parts[0]), base: normalizeUrl(parts[1]) }
+}
+
+function createPathEntry (value, mode) {
+  const parts = value.split(':')
+  if (parts.length === 1) {
+    return { mode, path: '/', base: normalizePath(parts[0]) }
+  }
+  return { mode, path: normalizeEntryPath(parts[0]), base: normalizePath(parts[1]) }
+}
+
+function isArray (value) {
+  return Object.prototype.toString.call(value) === '[object Array]'
+}
+
+function isNumber (value) {
+  return typeof value === 'number'
+}
+
+function normalizeEntryPath (path) {
+  return path.indexOf('/') ? `/${path}` : path
+}
+
+function normalizeUrl (url) {
+  return url.indexOf('http') ? `http://${url}` : url
+}
+
+function normalizePath (pathSegment) {
+  return path.resolve(pathSegment)
+}
