ff208ffefa8f5b6a5731d775e238ac8736d6c966
Bao Dang <baocancode@gmail.com>
2017-09-04 21:07:33 +1000

    refactor: clean code for mono-scripts

--- packages/mono-scripts/lib/cli.js
+++ packages/mono-scripts/lib/cli.js
@@ -5,11 +5,11 @@ const fs = require('fs-extra')
 
 const monoScripts = require('.')
 
-runScript()
+runScript().catch(console.error)
 
 async function runScript () {
   const rootPackageInfo = await fs.readJson(path.resolve('package.json'))
-  const config = rootPackageInfo.config.mono
+  const config = rootPackageInfo.config.monoScripts || rootPackageInfo.config.mono
   const exitCode = await monoScripts(process.argv.slice(2), config)
   process.exit(exitCode || 0)
 }
